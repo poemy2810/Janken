980d4e57e6fbc93237a57d73089bb4a138e4d486
poemy_Okamoto <57334338+poemy2810@users.noreply.github.com>
2019-12-30 19:32:45 +0900

    fourth commit

--- app/src/main/java/com/android/lifeistech/janken/MainActivity.kt
+++ app/src/main/java/com/android/lifeistech/janken/MainActivity.kt
@@ -2,7 +2,7 @@ package com.android.lifeistech.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         var hpPlayer = 5
         var hpCpu = 5
 
@@ -21,25 +22,31 @@ class MainActivity : AppCompatActivity() {
 
         progressBar_cpu.max = 5
         progressBar_cpu.progress = hpCpu
+        //Player,CPUのHPを設定
+
+
+        val win: Int = 0
+        val lose: Int = 0
+        val draw: Int = 0
+        //勝ち数、負け数、引き分け数の初期値を設定
+
+        val round: Int = 0
 
 
 
 
-        for (round in 1..5) {
-            battleTime.text = "$round 戦目"
+        battleTime.text = "$round 戦目"
+            //何戦目なのかを表示
 
-            val randomNum = Random().nextInt(3)
+        val randomNum: Int = Random().nextInt(3)
+            //ランダムな数を生成、コンピュータの出し手を決める
 
+            //0:"グー"
+            //1:"パー"
+            //2:"チョキ"
+        choki.setOnClickListener {
 
-            val win: Int = 0
-            val lose: Int = 0
-            val draw: Int = 0
-
-
-
-            choki.setOnClickListener {
-
-                player_hand.setImageResource(R.drawable.choki_me)
+            player.setImageResource(R.drawable.choki_me)
 
                 if (randomNum == 0) {
                     //相手の手がグーの時
@@ -47,7 +54,7 @@ class MainActivity : AppCompatActivity() {
                     resultThisTime.text = "まけ！！！！"
                     val lose = lose + 1
                     resultLose.text = "$lose 敗"
-                    progressBar_player.progress -= hpPlayer
+                    progressBar_player.progress = hpPlayer - 1
 
                 } else if (randomNum == 1) {
                     //相手の手がパーの時
@@ -56,7 +63,8 @@ class MainActivity : AppCompatActivity() {
 
                     val win = win + 1
                     resultWin.text = "$win 勝"
-                   // progressBar_cpu.secondaryProgress = 4
+                    progressBar_cpu.progress = hpCpu - 1
+
 
                 } else {
                     //相手の手がチョキの時
@@ -66,12 +74,12 @@ class MainActivity : AppCompatActivity() {
                     resultDraw.text = "$draw 引き分け"
                 }
 
-            }
+        }
 
 
-            gu.setOnClickListener {
+        gu.setOnClickListener {
 
-                player_hand.setImageResource(R.drawable.gu_me)
+                player.setImageResource(R.drawable.gu_me)
 
                 if (randomNum == 0) {
                     //相手の手がグーの時
@@ -86,6 +94,7 @@ class MainActivity : AppCompatActivity() {
                     resultThisTime.text = "まけ！！"
                     val lose = lose + 1
                     resultLose.text = "$lose 敗"
+                    progressBar_player.progress = hpPlayer - 1
 
                 } else {
                     //相手の手がチョキの時
@@ -93,19 +102,21 @@ class MainActivity : AppCompatActivity() {
                     resultThisTime.text = "あなたのかち！！"
                     val win = win + 1
                     resultWin.text = "$win 勝"
+                    progressBar_cpu.progress = hpCpu - 1
                 }
-            }
+        }
 
 
-            pa.setOnClickListener {
+        pa.setOnClickListener {
 
-                player_hand.setImageResource(R.drawable.pa_me)
+                player.setImageResource(R.drawable.pa_me)
                 if (randomNum == 0) {
                     //相手の手がグーの時
                     cpu.setImageResource(R.drawable.gu_aite)
                     resultThisTime.text = "あなたのかち！！"
                     val win = win + 1
                     resultWin.text = "$win 勝"
+                    progressBar_cpu.progress = hpCpu - 1
 
                 } else if (randomNum == 1) {
                     //相手の手がパーの時
@@ -115,20 +126,105 @@ class MainActivity : AppCompatActivity() {
                     resultDraw.text = "$draw 引き分け"
                 } else {
                     //相手の手がチョキの時
-                    cpu.setImageResource(R.drawable.pa_aite)
+                    cpu.setImageResource(R.drawable.choki_aite)
                     resultThisTime.text = "まけ！！"
                     val lose = lose + 1
                     resultLose.text = "$lose 敗"
+                    progressBar_player.progress = hpPlayer - 1
 
                 }
-
-            }
 
         }
 
 
+
+        /*
+        gu.setOnClickListener(listener)
+        pa.setOnClickListener(listener)
+        choki.setOnClickListener(listener)
+
+        */
+
     }
 }
+/*
+    val listener = object : View.OnClickListener {
+        override fun onClick(v: View?) {
+
+            var hands = IntArray(3)
+
+            hands[0] = R.drawable.gu_me
+            hands[1] = R.drawable.choki_me
+            hands[2] = R.drawable.pa_me
+
+            var handsCpu = IntArray(3)
+            handsCpu[0] = R.drawable.gu_aite
+            handsCpu[1] = R.drawable.choki_aite
+            handsCpu[2] = R.drawable.pa_aite
+
+
+            val round: Int = 0
+
+            var hand = 0
+            if (v == findViewById(R.id.gu)) {
+                hand = 0
+            } else if (v == findViewById((R.id.choki))) {
+                hand = 1
+            } else if (v == findViewById(R.id.pa)) {
+                hand = 2
+            }
+
+
+            var hpPlayer = 5
+            var hpCpu = 5
+
+            progressBar_player.max = 5
+            progressBar_player.progress = hpPlayer
+
+            progressBar_cpu.max = 5
+            progressBar_cpu.progress = hpCpu
+            //Player,CPUのHPを設定
+
+
+            val win: Int = 0
+            val lose: Int = 0
+            val draw: Int = 0
+            //勝ち数、負け数、引き分け数の初期値を設定
+
+            while (win < 4 || lose < 4) {
+                val random = Random().nextInt(3)
+                player.setImageResource(hands[hand])
+                cpu.setImageResource(handsCpu[random])
+
+
+                val shouhai = hand - random + 3
+
+
+
+
+                if (shouhai % 3 == 0) {
+                    resultThisTime.text = "あいこ！！！！"
+                    val draw = draw + 1
+                    resultDraw.text = "$draw 引き分け"
+
+                } else if (shouhai % 3 == 1) {
+                    resultThisTime.text = "まけ！！！！"
+                    val lose = lose + 1
+                    resultLose.text = "$lose 敗"
+                    progressBar_player.progress = hpPlayer - 1
+
+                } else if (shouhai % 3 == 2) {
+                    resultThisTime.text = "あなたのかち！！"
+                    val win = win + 1
+                    resultWin.text = "$win 勝"
+                    progressBar_cpu.progress = hpCpu - 1
+
+                }
+            }
+        }*/
+
+
+
 
 
 
